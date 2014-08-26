31c537ff42c1ccfe9cca3578cb47a1e380c788ad
ryou <ryou_i-12a@stu.kbc.ac.jp>
2014-08-26 11:57:06 +0900

    test

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Group3App::Application.config.secret_key_base = 'f1452aae2a877e10f878c48fee371755cc6273e66c4a8fe194002098600e255054296452cfb02957733f458d534332d96572fb0f29ba09f992cc220e0a794df6'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Group3App::Application.config.session_store :cookie_store, key: '_group3_app_session'
